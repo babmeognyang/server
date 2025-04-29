9c8a2ac6bec7c62d6d05fdeeb4a44e017e12f34f
ggamD00 <jaehuek@naver.com>
2025-04-29 10:06:54 +0900

    Initial commit

---
+++ src/main/java/com/example/babmeognyangserver/BabmeognyangServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.babmeognyangserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BabmeognyangServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BabmeognyangServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/babmeognyangserver/BabmeognyangServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.babmeognyangserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BabmeognyangServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
